a606ad02795ef5f3a69bc7a8476ffe11520d37e7
Rastsislau Yushkevich <rastsislauyushkevich@gmail.com>
2020-11-20 01:14:16 +0300

    PC + mobile fixes

--- js/main.js
+++ js/main.js
@@ -3,18 +3,11 @@ $('.home-slider').slick({
     slidesToScroll: 1,
     arrows: true,
     responsive: [{
-            breakpoint: 1140,
-            settings: {
-                arrows: false,
-            }
-        },
-        {
-            breakpoint: 1000,
-            settings: {
-                dots: false,
-            }
+        breakpoint: 1140,
+        settings: {
+            arrows: false,
         }
-    ],
+    }],
 
 });
 $('.about-slider').slick({
@@ -26,7 +19,13 @@ $('.testimonials-slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
-    dots: true
+    dots: true,
+    responsive: [{
+        breakpoint: 1000,
+        settings: {
+            dots: false,
+        }
+    }]
 });
 
 function myFunction() {
@@ -37,3 +36,103 @@ function myFunction() {
         x.style.display = "block";
     }
 }
+
+
+$("#top").on("click", "a", function(event) {
+    event.preventDefault();
+    let id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+});
+
+$("#home").on("click", "a", function(event) {
+    event.preventDefault();
+    let id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+});
+
+$("#about-us").on("click", "a", function(event) {
+    event.preventDefault();
+    let id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+});
+
+$("#services").on("click", "a", function(event) {
+    event.preventDefault();
+    let id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+});
+
+$("#portfolio").on("click", "a", function(event) {
+    event.preventDefault();
+    let id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+});
+
+$("#mission").on("click", "a", function(event) {
+    event.preventDefault();
+    let id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+});
+
+$("#qualities").on("click", "a", function(event) {
+    event.preventDefault();
+    let id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+});
+
+
+$("#testimonials").on("click", "a", function(event) {
+    event.preventDefault();
+    let id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+});
+
+$("#chess").on("click", "a", function(event) {
+    event.preventDefault();
+    let id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+});
+
+$("#app").on("click", "a", function(event) {
+    event.preventDefault();
+    let id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+});
+
+$("#events").on("click", "a", function(event) {
+    event.preventDefault();
+    let id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+});
+
+$("#team").on("click", "a", function(event) {
+    event.preventDefault();
+    let id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+});
+
+$("#newsletter").on("click", "a", function(event) {
+    event.preventDefault();
+    let id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+});
+
+$("#footer").on("click", "a", function(event) {
+    event.preventDefault();
+    let id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+});
